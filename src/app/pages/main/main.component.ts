03ca7e7f23c530df86387c3be19a6bcff9f0b31b
wenyin.tao <wenyin.tao@163.com>
2018-12-11 23:15:03 +0800

    添加首页的版权内容，并初始化主页的左边菜单和内容结构

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  // 默认是否折叠
+  public isCollapsed = false;
+  public isReverseArrow = false;
+  // 菜单栏的默认宽度
+  public menuWidth = 200;
+
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgZorroAntdModule
   ]
 })
 export class MainModule { }
