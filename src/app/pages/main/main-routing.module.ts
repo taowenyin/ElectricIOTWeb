b261a15df66fd0e3fc41fead0285709b910578a2
陶文寅 <wenyin.tao@163.com>
2018-12-14 18:22:37 +0800

    自动跳转到地图页面

--- src/app/pages/main/main-routing.module.ts
+++ src/app/pages/main/main-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class MainRoutingModule { }
